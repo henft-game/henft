fd53b1965fadff0757eeec2522c377e6f90feb66
Rafael Faita <rfaita@DESKTOP-O4B4QH6.localdomain>
2021-12-29 16:00:10 -0300

    home page

--- dapp/src/pages/Roadmap.js
+++ dapp/src/pages/Roadmap.js
@@ -17,8 +17,27 @@ const Roadmap = () => {
         },
     }));
 
+    const CustomHelpIcon = styled(HelpIcon)(({ theme }) => ({
+        '&&': {
+            position: 'absolute',
+            zIndex: '1000',
+            fontSize: '3vw',
+        },
+        [theme.breakpoints.down('sm')]: {
+            '&&': {
+                fontSize: '5vw !important',
+            }
+        },
+        [theme.breakpoints.up('xl')]: {
+            '&&': {
+                fontSize: '2vw !important',
+            }
+        },
+    }));
+
+
     const CustomTooltip = styled(({ className, ...props }) => (
-        <Tooltip {...props} classes={{ popper: className }} enterTouchDelay={0}/>
+        <Tooltip {...props} classes={{ popper: className }} enterTouchDelay={0} />
     ))({
         [`& .${tooltipClasses.tooltip}`]: {
             lineHeight: '24px',
@@ -33,28 +52,28 @@ const Roadmap = () => {
                 <Grid item sx={{ margin: 'auto', padding: '10px', borderRadius: '4px', position: 'relative' }} xs={12} md={10} lg={8} xl={6}>
                     <Img src="imgs/roadmap.png" alt="roadmap" />
                     <CustomTooltip title="A new page will be added, it will contain the battle history, the leaderboard of the current month and the history of previous champions.">
-                        <HelpIcon sx={{ position: 'absolute', top: '9%', left: '61%', zIndex: '1000', fontSize: '3vw' }} />
+                        <CustomHelpIcon sx={{ top: '9%', left: '61%', }} />
                     </CustomTooltip>
                     <CustomTooltip title="">
-                        <HelpIcon sx={{ position: 'absolute', top: '18.7%', left: '92%', zIndex: '1000', fontSize: '3vw' }} />
+                        <CustomHelpIcon sx={{ top: '18.7%', left: '92%', }} />
                     </CustomTooltip>
                     <CustomTooltip title="A whitepaper will be added with all the details and plans for season 1 and intentions for upcoming seasons.">
-                        <HelpIcon sx={{ position: 'absolute', top: '30.8%', left: '60.5%', zIndex: '1000', fontSize: '3vw' }} />
+                        <CustomHelpIcon sx={{ top: '30.8%', left: '60.5%', }} />
                     </CustomTooltip>
                     <CustomTooltip title="A new page will be added explaining all the calculations and rules of the game.">
-                        <HelpIcon sx={{ position: 'absolute', top: '42.5%', left: '41.5%', zIndex: '1000', fontSize: '3vw' }} />
+                        <CustomHelpIcon sx={{ top: '42.5%', left: '41.5%', }} />
                     </CustomTooltip>
                     <CustomTooltip title="Equipment system will be added in the game, allowing Hens to use equipment that will give bonuses during battles.">
-                        <HelpIcon sx={{ position: 'absolute', top: '54%', left: '46.5%', zIndex: '1000', fontSize: '3vw' }} />
+                        <CustomHelpIcon sx={{ top: '54%', left: '46.5%', }} />
                     </CustomTooltip>
                     <CustomTooltip title="Total of 200 unique hens ready.">
-                        <HelpIcon sx={{ position: 'absolute', top: '73.8%', left: '43%', zIndex: '1000', fontSize: '3vw' }} />
+                        <CustomHelpIcon sx={{ top: '73.8%', left: '43%', }} />
                     </CustomTooltip>
                     <CustomTooltip title="Statistics related to HeNFT will be added to the home page.">
-                        <HelpIcon sx={{ position: 'absolute', top: '66.6%', left: '87%', zIndex: '1000', fontSize: '3vw' }} />
+                        <CustomHelpIcon sx={{ top: '66.6%', left: '87%', }} />
                     </CustomTooltip>
                     <CustomTooltip title="A new game feature will be added, this will allow players to 'rent' their Hens, more details will be added with whitepaper.">
-                        <HelpIcon sx={{ position: 'absolute', top: '84.5%', left: '55%', zIndex: '1000', fontSize: '3vw' }} />
+                        <CustomHelpIcon sx={{ top: '84.5%', left: '55%', }} />
                     </CustomTooltip>
 
                 </Grid>
