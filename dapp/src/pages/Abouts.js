e4408582f2190eb40d95e4b2e35e385af6c426d8
Rafael Faita <rfaita@DESKTOP-O4B4QH6.localdomain>
2021-12-30 14:13:47 -0300

    home page

--- dapp/src/pages/Abouts.js
+++ dapp/src/pages/Abouts.js
@@ -9,6 +9,10 @@ const About = () => {
         lineHeight: '22px'
     }));
 
+    const Title = styled('h2')(({ theme }) => ({
+        marginTop: '30px'
+    }));
+
     const Img = styled('img')(({ theme }) => ({
         maxWidth: '320px',
         width: '100%',
@@ -22,12 +26,26 @@ const About = () => {
         },
     }));
 
+    const NormalDiv = styled('div')(({ theme }) => ({
+        display: 'block',
+        [theme.breakpoints.down('sm')]: {
+            display: 'none',
+        },
+    }));
+
+    const MobileDiv = styled('div')(({ theme }) => ({
+        display: 'none',
+        [theme.breakpoints.down('sm')]: {
+            display: 'block',
+        },
+    }));
+
 
     return (
         <AboutBox>
             <Grid container>
                 <Grid item sx={{ margin: 'auto', background: '#DCC1A1', border: '4px solid #61422D', padding: '10px', borderRadius: '4px' }} xs={12} md={10} lg={8} xl={6}>
-                    <h3>The Project</h3>
+                    <Title style={{ marginTop: 0 }}>The Project</Title>
 
                     <Text>
                         HeNFT is a NFT game that uses ERC 721 Standard and has all the code in the block chain,
@@ -39,7 +57,7 @@ const About = () => {
                         the price in a fair amount.
                     </Text>
 
-                    <h3>The Game</h3>
+                    <Title>The Game</Title>
 
                     <Text>
                         In HeNFT players are able to fight using the Hens that they hold, improve their levels and for every
@@ -48,7 +66,7 @@ const About = () => {
                         gas fees and values that are reverted to the game or players.
                     </Text>
 
-                    <h3>Why to Buy a HeNFT?</h3>
+                    <Title>Why to Buy a HeNFT?</Title>
 
                     <Text>
                         People can buy HeNFT for three different reasons, first as a way of investment, the value of a hens
@@ -58,7 +76,7 @@ const About = () => {
                         in our art and ideas.
                     </Text>
 
-                    <h3>Can I have multiple Hens?</h3>
+                    <Title>Can I have multiple Hens?</Title>
 
                     <Text>
                         Yes, they are limited but a single person can hold multiple hens, to avoid that enthusiasts buy
@@ -66,7 +84,7 @@ const About = () => {
                         new hens and sometimes selling in different ways and many times doing giveaways.
                     </Text>
 
-                    <h3>The team</h3>
+                    <Title>The team</Title>
 
                     <Text>
                         We are two members and one extra help artist, we have our works and other tasks to keep with,
@@ -150,58 +168,111 @@ const About = () => {
                     </Grid>
 
 
-                    <h3>How are the Hens made?</h3>
+                    <Title>How are the Hens made?</Title>
 
+                    <NormalDiv>
+                        <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
+                            <Grid item xs={12} md sx={{ textAlign: 'right' }}>
+                                <Text>1. We have a big list of cool ideas and historical things,</Text>
+                            </Grid>
+                            <Grid item xs={12} md sx={{ textAlign: 'left' }}>
+                                <Img src="imgs/eggnext.gif" alt="egg next" />
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
+                            <Grid item xs={12} md sx={{ textAlign: 'right' }}>
+                                <Img src="imgs/new_hen.gif" alt="new hen" />
+                            </Grid>
+                            <Grid item xs={12} md sx={{ textAlign: 'left' }}>
+                                <Text>2. We pick one idea from there and draw a friendly hen,</Text>
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
+                            <Grid item xs={12} md sx={{ textAlign: 'right' }}>
+                                <Text>3. We share with the other members, friends and family to get reviews,</Text>
+                            </Grid>
+                            <Grid item xs={12} md sx={{ textAlign: 'left' }}>
+                                <Img src="imgs/changes.gif" alt="changes" />
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
+                            <Grid item xs={12} md sx={{ textAlign: 'right' }}>
+                                <Img src="imgs/rarity.gif" alt="rarity" />
+                            </Grid>
+                            <Grid item xs={12} md sx={{ textAlign: 'left' }}>
+                                <Text>4.We define the rarity of that hen based on how nice and famous it is and how hard was to do,</Text>
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
+                            <Grid item xs={12} md sx={{ textAlign: 'right' }}>
+                                <Text>5.We generate random attributes using one small application that we developed,</Text>
+                            </Grid>
+                            <Grid item xs={12} md sx={{ textAlign: 'left' }}>
+                                <Img src="imgs/attributes.gif" alt="attributes" />
+                            </Grid>
+                        </Grid>
 
-                    <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
-                        <Grid item xs={12} md sx={{ textAlign: 'right' }}>
-                            <Text>1. We have a big list of cool ideas and historical things,</Text>
+                        <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
+                            <Grid item xs={12} md sx={{ textAlign: 'right' }}>
+                                <Img src="imgs/eggBox.gif" alt="rarity" />
+                            </Grid>
+                            <Grid item xs={12} md sx={{ textAlign: 'left' }}>
+                                <Text>6.We wait for a total of 8 hens finished and finally we deliver it to you!</Text>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} md sx={{ textAlign: 'left' }}>
-                            <Img src="imgs/eggnext.gif" alt="egg next" />
-                        </Grid>
-                    </Grid>
-                    <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
-                        <Grid item xs={12} md sx={{ textAlign: 'right' }}>
-                            <Img src="imgs/new_hen.gif" alt="new hen" />
-                        </Grid>
-                        <Grid item xs={12} md sx={{ textAlign: 'left' }}>
-                            <Text>2. We pick one idea from there and draw a friendly hen,</Text>
-                        </Grid>
-                    </Grid>
-                    <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
-                        <Grid item xs={12} md sx={{ textAlign: 'right' }}>
-                            <Text>3. We share with the other members, friends and family to get reviews,</Text>
-                        </Grid>
-                        <Grid item xs={12} md sx={{ textAlign: 'left' }}>
-                            <Img src="imgs/changes.gif" alt="changes" />
-                        </Grid>
-                    </Grid>
-                    <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
-                        <Grid item xs={12} md sx={{ textAlign: 'right' }}>
-                            <Img src="imgs/rarity.gif" alt="rarity" />
-                        </Grid>
-                        <Grid item xs={12} md sx={{ textAlign: 'left' }}>
-                            <Text>4.We define the rarity of that hen based on how nice and famous it is and how hard was to do,</Text>
-                        </Grid>
-                    </Grid>
-                    <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
-                        <Grid item xs={12} md sx={{ textAlign: 'right' }}>
-                            <Text>5.We generate random attributes using one small application that we developed,</Text>
-                        </Grid>
-                        <Grid item xs={12} md sx={{ textAlign: 'left' }}>
-                            <Img src="imgs/attributes.gif" alt="attributes" />
-                        </Grid>
-                    </Grid>
+                    </NormalDiv>
 
-                    <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
-                        <Grid item xs={12} md sx={{ textAlign: 'right' }}>
-                            <Img src="imgs/eggBox.gif" alt="rarity" />
+                    <MobileDiv>
+                        <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
+                            <Grid item xs={12} md>
+                                <Text>1. We have a big list of cool ideas and historical things,</Text>
+                            </Grid>
+                            <Grid item xs={12} md>
+                                <Img src="imgs/eggnext.gif" alt="egg next" />
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} md sx={{ textAlign: 'left' }}>
-                            <Text>6.We wait for a total of 8 hens finished and finally we deliver it to you!</Text>
+                        <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
+                            <Grid item xs={12} md>
+                                <Text>2. We pick one idea from there and draw a friendly hen,</Text>
+                            </Grid>
+                            <Grid item xs={12} md>
+                                <Img src="imgs/new_hen.gif" alt="new hen" />
+                            </Grid>
                         </Grid>
-                    </Grid>
+                        <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
+                            <Grid item xs={12} md>
+                                <Text>3. We share with the other members, friends and family to get reviews,</Text>
+                            </Grid>
+                            <Grid item xs={12} md>
+                                <Img src="imgs/changes.gif" alt="changes" />
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
+                            <Grid item xs={12} md>
+                                <Text>4.We define the rarity of that hen based on how nice and famous it is and how hard was to do,</Text>
+                            </Grid>
+                            <Grid item xs={12} md>
+                                <Img src="imgs/rarity.gif" alt="rarity" />
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
+                            <Grid item xs={12} md>
+                                <Text>5.We generate random attributes using one small application that we developed,</Text>
+                            </Grid>
+                            <Grid item xs={12} md>
+                                <Img src="imgs/attributes.gif" alt="attributes" />
+                            </Grid>
+                        </Grid>
+
+                        <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
+                            <Grid item xs={12} md>
+                                <Text>6.We wait for a total of 8 hens finished and finally we deliver it to you!</Text>
+                            </Grid>
+                            <Grid item xs={12} md>
+                                <Img src="imgs/eggBox.gif" alt="rarity" />
+                            </Grid>
+                        </Grid>
+                    </MobileDiv>
                 </Grid>
             </Grid>
 
