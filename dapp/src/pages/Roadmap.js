35fc9305f4605feb51ecce618657b49d5720f986
Rafael Faita <rfaita@DESKTOP-O4B4QH6.localdomain>
2022-03-22 10:24:42 -0300

    fixing roadmap helpers position

--- dapp/src/pages/Roadmap.js
+++ dapp/src/pages/Roadmap.js
@@ -64,29 +64,29 @@ const Roadmap = () => {
             <Grid container>
                 <Grid item sx={{ margin: 'auto', padding: '10px', borderRadius: '4px', position: 'relative' }} xs={12} md={10} lg={8} xl={6}>
                     <Img src="imgs/roadmap.png" alt="roadmap" />
+                    <CustomTooltip title="A whitepaper will be added with all the details and plans for season 1 and intentions for upcoming seasons.">
+                        <CustomHelpIcon sx={{ top: '9%', left: '68%', }}>?</CustomHelpIcon>
+                    </CustomTooltip>
                     <CustomTooltip title="The items marketplace for usable items will be added allowing users to sell and buy.">
-                        <CustomHelpIcon sx={{ top: '9%', left: '61%', }}>?</CustomHelpIcon>
+                        <CustomHelpIcon sx={{ top: '19.0%', left: '92%', }}>?</CustomHelpIcon>
                     </CustomTooltip>
                     <CustomTooltip title="A new page will be added, it will contain the battle history, the leaderboard of the current month and the history of previous champions.">
-                        <CustomHelpIcon sx={{ top: '18.7%', left: '92%', }}>?</CustomHelpIcon>
-                    </CustomTooltip>
-                    <CustomTooltip title="A whitepaper will be added with all the details and plans for season 1 and intentions for upcoming seasons.">
-                        <CustomHelpIcon sx={{ top: '30.8%', left: '60.5%', }}>?</CustomHelpIcon>
+                        <CustomHelpIcon sx={{ top: '30.8%', left: '60%', }}>?</CustomHelpIcon>
                     </CustomTooltip>
                     <CustomTooltip title="A new page will be added explaining all the calculations and rules of the game.">
-                        <CustomHelpIcon sx={{ top: '42.5%', left: '41.5%', }}>?</CustomHelpIcon>
+                        <CustomHelpIcon sx={{ top: '42.5%', left: '41%', }}>?</CustomHelpIcon>
                     </CustomTooltip>
                     <CustomTooltip title="Equipment system will be added in the game, allowing Hens to use equipment that will give bonuses during battles.">
                         <CustomHelpIcon sx={{ top: '54%', left: '46.5%', }}>?</CustomHelpIcon>
                     </CustomTooltip>
                     <CustomTooltip title="Total of 200 unique hens ready.">
-                        <CustomHelpIcon sx={{ top: '73.8%', left: '43%', }}>?</CustomHelpIcon>
+                        <CustomHelpIcon sx={{ top: '73.8%', left: '48%', }}>?</CustomHelpIcon>
                     </CustomTooltip>
                     <CustomTooltip title="Statistics related to HeNFT will be added to the home page.">
-                        <CustomHelpIcon sx={{ top: '66.6%', left: '87%', }}>?</CustomHelpIcon>
+                        <CustomHelpIcon sx={{ top: '66.6%', left: '94%', }}>?</CustomHelpIcon>
                     </CustomTooltip>
                     <CustomTooltip title="A new game feature will be added, this will allow players to 'rent' their Hens, more details will be added with whitepaper.">
-                        <CustomHelpIcon sx={{ top: '84.5%', left: '55%', }}>?</CustomHelpIcon>
+                        <CustomHelpIcon sx={{ top: '84.5%', left: '58%', }}>?</CustomHelpIcon>
                     </CustomTooltip>
 
                 </Grid>
